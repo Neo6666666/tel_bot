838ab5480fc7d5dee83fd4faa5336b297609530d
Spiridenko Pavel <spiridenko_po@kuzro.ru>
2019-11-26 15:33:54 +0700

    bugfix

--- bot/birth_day.py
+++ bot/birth_day.py
@@ -5,14 +5,13 @@ class BirthDay:
     @staticmethod
     def get_birth_days():
         curr_date = datetime.datetime.now()
-        persons = {}
-        message = 'Сегодня никаких уведомлений.'
+        message = []
         with open('dates.csv', 'r') as f:
             for s in f:
                 arr = s.split(';')
                 birth_date = arr[0][1:-1].split('.')
                 if int(birth_date[0]) == int(curr_date.day) \
                         and int(birth_date[1]) == int(curr_date.month):
-                    message = arr[1]
+                    message.append(arr[1])
 
-        return f'На {curr_date.day}.{curr_date.month}.{curr_date.year}: {message}'
+        return f'На {curr_date.day}.{curr_date.month}.{curr_date.year}: {','.join(message) if message else 'Сегодня никаких уведомлений.'}'
