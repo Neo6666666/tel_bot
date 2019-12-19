9f29a344684666f629a9d060d8e0209f215f4dc2
Spiridenko Pavel <spiridenko_po@kuzro.ru>
2019-12-19 14:34:39 +0700

    some checks

--- bot/birth_day.py
+++ bot/birth_day.py
@@ -6,12 +6,18 @@ class BirthDay:
     def get_birth_days():
         curr_date = datetime.datetime.now()
         message = []
-        with open('dates.csv', 'r') as f:
+        with open('dates2.csv', 'r') as f:
             for s in f:
                 arr = s.split(';')
                 birth_date = arr[0][1:-1].split('.')
-                if int(birth_date[0]) == int(curr_date.day) \
-                        and int(birth_date[1]) == int(curr_date.month):
+                print(birth_date)
+                print(int(birth_date[0]), int(curr_date.day))
+                print(int(birth_date[1]), int(curr_date.month))
+                if int(birth_date[0]) == int(curr_date.day) and int(birth_date[1]) == int(curr_date.month):
                     message.append(arr[1])
 
         return f'На {curr_date.day}.{curr_date.month}.{curr_date.year}: {",".join(message) if message else "Сегодня никаких уведомлений."}'
+
+
+if __name__ == '__main__':
+    BirthDay.get_birth_days()
