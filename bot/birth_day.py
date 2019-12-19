de7075ab0fbef39ff02ee06ff7c8a05892a0b8c8
Spiridenko Pavel <spiridenko_po@kuzro.ru>
2019-12-19 14:40:55 +0700

    some checks 2

--- bot/birth_day.py
+++ bot/birth_day.py
@@ -6,7 +6,7 @@ class BirthDay:
     def get_birth_days():
         curr_date = datetime.datetime.now()
         message = []
-        with open('dates2.csv', 'r') as f:
+        with open('dates.csv', 'r') as f:
             for s in f:
                 arr = s.split(';')
                 birth_date = arr[0][1:-1].split('.')
